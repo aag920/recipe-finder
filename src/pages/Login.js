3bed31f7861580d7348caafaeec572f7215e50fa
Alyssa Gramling <alyssagramling55@gmail.com>
2025-11-19 09:48:45 -0500

    finished styling login and register page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,19 +17,33 @@ function Login() {
     setLoginData({ ...loginData, [e.target.name]: e.target.value });
   };
   return (
-    <div>
-      <h1>Login here</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input type="text" name="email" onChange={handleInput} />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" onChange={handleInput} />
-        </label>
-        <button>Submit</button>
-      </form>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Login here</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label className="login-label">
+            Email
+            <input
+              className="login-input"
+              type="text"
+              name="email"
+              onChange={handleInput}
+              placeholder="Enter your email"
+            />
+          </label>
+          <label className="login-label">
+            Password
+            <input
+              className="login-input"
+              type="password"
+              name="password"
+              onChange={handleInput}
+              placeholder="Enter your password"
+            />
+          </label>
+          <button className="login-button">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -17,19 +17,33 @@ function Register() {
     setRegisterData({ ...registerData, [e.target.name]: e.target.value });
   };
   return (
-    <div>
-      <h1>Register here</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input type="text" name="email" onChange={handleInput} />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" onChange={handleInput} />
-        </label>
-        <button>Submit</button>
-      </form>
+    <div className="register-container">
+      <div className="register-box">
+        <h1>Register here</h1>
+        <form className="register-form" onSubmit={handleSubmit}>
+          <label className="register-label">
+            Email
+            <input
+              className="register-input"
+              type="text"
+              name="email"
+              onChange={handleInput}
+              placeholder="Enter your email"
+            />
+          </label>
+          <label className="register-label">
+            Password
+            <input
+              className="register-input"
+              type="password"
+              name="password"
+              onChange={handleInput}
+              placeholder="Create a password"
+            />
+          </label>
+          <button className="register-button">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
