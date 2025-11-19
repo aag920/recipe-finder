2293c0b8c0b57e71dd6ecef9097e31b965a673ef
Alyssa Gramling <alyssagramling55@gmail.com>
2025-11-19 14:12:52 -0500

    added logout button

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }, []);
   return (
     <>
-      <Navbar token={token} />
+      <Navbar token={token} setToken={setToken} />
       <Routes>
         <Route path="/" element={<Homepage token={token} />} />
         <Route
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-export default function Navbar({ token }) {
+export default function Navbar({ token, setToken }) {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
   return (
     <nav>
       <Link className="nav-logo" to="/">
@@ -11,6 +15,7 @@ export default function Navbar({ token }) {
       <div className="nav-links">
         {!token && <Link to="/login">Login</Link>}
         {!token && <Link to="/register">Register</Link>}
+        {token && <button onClick={handleLogout}>Logout</button>}
       </div>
     </nav>
   );
